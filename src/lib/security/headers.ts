9f49e61974412be9df41508ca78cd6c7db7b9e16
Aryamann Sharma <aryamann002@gmail.com>
2026-07-10 23:37:31 +0530

    security fix #2

--- src/lib/security/headers.ts
+++ src/lib/security/headers.ts
@@ -6,7 +6,7 @@
 export const SECURITY_HEADERS: Record<string, string> = {
   'Content-Security-Policy': [
     "default-src 'self'",
-    "script-src 'self' 'unsafe-inline'",
+    "script-src 'self'",
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
     "font-src 'self' https://fonts.gstatic.com",
     "img-src 'self' data: blob:",
